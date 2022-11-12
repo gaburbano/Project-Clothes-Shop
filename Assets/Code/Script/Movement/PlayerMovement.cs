e3206f3f87372f13c3b15b6147726d50f80af662
GAB <jgaburbano@gmail.com>
2022-11-12 11:21:24 +0800

    Implement basic player movement

---
+++ Assets/Code/Script/Movement/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    public Rigidbody2D rigidbody2D;
+
+    private Vector2 movement;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.MovePosition(rigidbody2D.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
